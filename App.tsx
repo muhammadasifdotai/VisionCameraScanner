cdea0c08eb348a8c11023b93e5dbca44bf7f7696
muhammadasifdotai <sethrolin2573@gmail.com>
2024-09-22 10:57:14 +0500

    Alhamdulillah project is working.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, Code, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera';
 
 export default function App(): JSX.Element {
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device = useCameraDevice('back')
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes: Code[]) => {
+      console.log(`Scanned ${codes[0].value} codes!`)
+    }
+  })
+
+  React.useEffect(() => {
+    requestPermission()
+  }, [])
+
+  if (device == null) {
+    return (
+      <View>
+        <Text>Device not found</Text>
+      </View>
+    )
+  }
   return (
-    <View style={styles.main}>
-      <Text style={styles.text}>Alhamdulillah</Text>
-    </View>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      codeScanner={codeScanner}
+    />
   )
 }
 
